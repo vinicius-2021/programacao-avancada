f0534ef18426b9981d115985e3ae92015b5348df
vinicius-2021 <81392814+vinicius-2021@users.noreply.github.com>
2021-08-04 21:57:17 -0300

    commit 1.0

---
+++ src/pkg1programacao/Main.java
@@ -0,0 +1,30 @@
+
+package pkg1programacao;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       // laço de repetição FOR -> Tabuada
+        int tabuada = 9;
+        for(int i = 0; i < 10; i++){
+            System.out.println(tabuada + "x" + i + " = " + (tabuada * i) );
+            
+        }
+        
+        //WHILE = Enquanto
+        
+        int contador = 0;
+        while(contador < 100){
+            System.out.println("Contador é menor que 100");
+        }
+         System.out.println("Terminou o while");
+         
+         //DO WHILE = Verificar um contador
+    }
+    //Criar uma função de soma
+    double a = 10.4;
+    double b = 6;
+
+
+}
